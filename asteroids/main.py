752fdcffcfe51a2c2de017815a546fc205f09952
Dylan Guest <dguest3@gmail.com>
2025-10-16 19:07:40 -0500

    CH2L4

--- asteroids/main.py
+++ asteroids/main.py
@@ -4,6 +4,8 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    game_clock = pygame.time.Clock()
+    dt = 0
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -15,7 +17,8 @@ def main():
                 running = False
                 return running
         pygame.Surface.fill(screen,(0, 0, 0))  # Clear screen with black
-        pygame.display.flip()
+        pygame.display.flip() 
+        dt = game_clock.tick(60) / 1000  # Limit to 60 FPS and get delta time in seconds
 
 if __name__ == "__main__":
     main()
