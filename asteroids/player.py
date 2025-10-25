04f3c95f1038770b13d30877953367e6c3f7b246
Dylan Guest <dguest3@gmail.com>
2025-10-25 08:42:40 -0500

    CH3L4

--- asteroids/player.py
+++ asteroids/player.py
@@ -7,6 +7,8 @@ class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+        self.timer = 0
+        #return timer
 
     # in the player class
     def triangle(self):
@@ -36,12 +38,20 @@ class Player(CircleShape):
             self.move(-dt)
         if keys[pygame.K_SPACE]:
             self.shoot()
+            self.timer -= dt
+        
+        
 
     def move(self, dt):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         self.position += forward * PLAYER_SPEED * dt
 
     def shoot(self):
+        if self.timer <= 0:
+            self.timer = PLAYER_SHOOT_COOLDOWN
+        else:
+            return
+        
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         shot_velocity = forward * PLAYER_SHOOT_SPEED
-        Shot(self.position, shot_velocity)
+        Shot(self.position, shot_velocity) 
